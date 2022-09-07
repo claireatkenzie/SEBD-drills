72a6a29cc9f8f548cae939a4022e9933390ce2c0
claireatkenzie <claire.munds@kenzie.academy>
2022-09-07 11:17:26 -0600

    Emptying main

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -2,8 +2,6 @@ package com.kenzie.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        // Edit the following line and put in your name
-        System.out.println("My name is YOUR NAME HERE");
+        System.out.println("Welcome to Drills!");
     }
 }
