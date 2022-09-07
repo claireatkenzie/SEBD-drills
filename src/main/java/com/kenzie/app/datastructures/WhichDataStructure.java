ccec3320ce908b1be35ebc3956dfece2d29eeb16
claireatkenzie <claire.munds@kenzie.academy>
2022-09-07 14:03:09 -0600

    Data structure - which alarm code

--- src/main/java/com/kenzie/app/datastructures/WhichDataStructure.java
+++ src/main/java/com/kenzie/app/datastructures/WhichDataStructure.java
@@ -1,25 +1,62 @@
 package com.kenzie.app.datastructures;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class WhichDataStructure {
     // DataStructureClass <Type(s)> name = new DataStructureClass<>();
 
-    // Days of the week
-    public static Object week() {
-        return null;
+
+    /**************************************************************************************
+     * Write the test first to make sure the following is returned from this method
+     * MONDAY TUESDAY WEDNESDAY THURSDAY FRIDAY SATURDAY SUNDAY
+     * After, debug this method
+     ***************************************************************************************/
+    public static ArrayList<String> createDaysOfWeek() {
+
+        ArrayList<String> daysOfWeek = new ArrayList<>();
+
+        daysOfWeek.add("SUNDAY");
+        daysOfWeek.add("MONDAY");
+        daysOfWeek.add("TUESDAY");
+        daysOfWeek.add("WEDNESDAY");
+        daysOfWeek.add("THURSDAY");
+        daysOfWeek.add("frIDAY");
+        daysOfWeek.add("SATURDAY");
+
+        return daysOfWeek;
     }
 
-    // Alarm time on each day of the week
+    /**************************************************************************************
+     * Alter this method to return the day to alarm lookup map
+     * You'll need to alter the test to use this value
+     ***************************************************************************************/
     public static Object alarmTime() {
+
+        HashMap<String, Integer> dayToAlarmTimeLookUp = new HashMap<>();
+        dayToAlarmTimeLookUp.put("MONDAY", 6);
+        dayToAlarmTimeLookUp.put("TUESDAY", 7);
+        dayToAlarmTimeLookUp.put("WEDNESDAY", 8);
+        dayToAlarmTimeLookUp.put("THURSDAY", 7);
+        dayToAlarmTimeLookUp.put("FRIDAY", 9);
+        dayToAlarmTimeLookUp.put("Saturday", 2);
+        dayToAlarmTimeLookUp.put("Sunday", 10);
+
         return null;
     }
 
-    // Find what time I should get up
-    public static Object lookupAlarm(String dayOfWeek) {
-        return null;
+    /**************************************************************************************
+     * Write a method that looks up the alarm based on the day of the week passed in
+     ***************************************************************************************/
+    public static void lookupAlarm(String dayOfWeek) {
+
     }
 
     public static void main(String[] args) {
-        System.out.println("we're practicing data structures!");
+        lookupAlarm("MONDAY");
+        lookupAlarm("monday");
+        lookupAlarm("wednsday");
     }
 
 }
--- src/test/java/com/kenzie/app/WhichDataStructureTest.java
+++ src/test/java/com/kenzie/app/WhichDataStructureTest.java
@@ -1,19 +1,56 @@
 package com.kenzie.app;
 
 
+import com.kenzie.app.datastructures.WhichDataStructure;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WhichDataStructureTest {
 
+    /**************************************************************************************
+     * Write a test that ensures that the data structure contains all days of the week
+     * MONDAY TUESDAY WEDNESDAY THURSDAY FRIDAY SATURDAY SUNDAY
+    ***************************************************************************************/
     @Test
-    void which1() {
+    void testWeek() {
+        // Arrange
+        // Act
+        // Assert
     }
 
     @Test
-    void which2() {
+    void testAlarmTime() {
+        // Arrange
+        HashMap<String, Integer> expectedData = new HashMap<>();
+        expectedData.put("MONDAY", 6);
+        expectedData.put("TUESDAY", 7);
+        expectedData.put("WEDNESDAY", 8);
+        expectedData.put("THURSDAY", 7);
+        expectedData.put("FRIDAY", 9);
+        expectedData.put("SATURDAY", 2);
+        expectedData.put("SUNDAY", 10);
+
+
+        // Act - what should this call be?
+        HashMap<String, Integer> actual = null;
+
+        // Assert
+        for(Map.Entry<String, Integer> day : expectedData.entrySet()) {
+            String key = day.getKey();
+            Integer expectedValue = day.getValue();
+            Integer actualValue = actual.get(key);
+            assert(expectedValue.equals(actualValue));
+        }
+
     }
 
+
+    /**************************************************************************************
+     * Write a test that asserts Monday return an alarm time of 6
+     ***************************************************************************************/
     @Test
-    void which3() {
+    void testLookUpAlarm() {
     }
 }
