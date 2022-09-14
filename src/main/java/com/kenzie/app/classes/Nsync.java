753c2720790de9f901de663662c747375218f58b
claireatkenzie <claire.munds@kenzie.academy>
2022-09-14 10:25:01 -0700

    fixing errors

--- src/main/java/com/kenzie/app/classes/Nsync.java
+++ src/main/java/com/kenzie/app/classes/Nsync.java
@@ -4,20 +4,5 @@ public class Nsync {
 
     public static void main(String[] args) {
 
-        Early2000sPopSinger hannahMontana = new Early2000sPopSinger();
-        hannahMontana.hair = "blonde";
-        hannahMontana.dna = "CATAGAGAGCDADDTATATATATA";
-
-        Early2000sPopSinger mileyCyrus = new Early2000sPopSinger();
-        mileyCyrus.hair = "brown";
-        mileyCyrus.dna = "CATAGAGAGCDADDTATATATATA";
-
-        System.out.println(hannahMontana.equals(mileyCyrus));
-
-
-
-
-
-
     }
 }
